836d66212139c77618a4515db0ff32707713ad34
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-21 09:49:23 +0300

    create context for tracking bookmarked articles

---
+++ src/context/bookmark-context.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState } from "react";
+
+export const BookmarkContext = createContext();
+
+export const BookmarkContextProvider = ({ children }) => {
+  const [savedArticles, setSavedArticles] = useState([]);
+
+  const addArticle = (article) =>
+    setSavedArticles((prev) => [...prev, article]);
+
+  const removeArticle = (articleId) => {
+    return setSavedArticles((prev) =>
+      prev.filter((article) => article.id !== articleId)
+    );
+  };
+
+  return (
+    <BookmarkContext.Provider
+      value={{
+        bookmarkedArticles: savedArticles,
+        bookmarkArticle: addArticle,
+        unbookmarkArticle: removeArticle,
+      }}
+    >
+      {children}
+    </BookmarkContext.Provider>
+  );
+};
+
+export const useBookmarkContext = () => {
+  return useContext(BookmarkContext);
+};
